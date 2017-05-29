672ae8bc82bde104e6772cd154f2eab05e94a4d9
Rafael Arenas Schuchowsky <rafael.arenas352@gmail.com>
2017-05-29 16:29:34 -0300

    Core services

---
+++ src/core/services/FetchService.js
@@ -0,0 +1,53 @@
+import { extendObservable, action } from 'mobx';
+import { browserHistory } from 'react-router';
+import get from 'lodash/get';
+import axios from 'axios';
+import qs from 'qs';
+
+class FetchService {
+  constructor() {
+    extendObservable(this, {
+      fetching: false,
+      response: null,
+      data: null,
+      error: null,
+      userId: null,
+    });
+  }
+
+  fetch = action((params) => {
+    if (!this.userId && localStorage.profile) {
+      const profile = JSON.parse(localStorage.profile);
+      this.userId = profile.user_id;
+    }
+    this.data = null;
+    this.fetching = true;
+    return axios.request({
+      method: params.method || 'get',
+      url: `${process.env.REACT_APP_API_URL}${params.url}`,
+      params: params.query,
+      paramsSerializer: params => qs.stringify(params, { arrayFormat: 'brackets' }),
+      data: params.body,
+      headers: {
+        ...params.headers,
+        ...localStorage.idToken && { Authorization: `Bearer ${localStorage.idToken}` },
+      },
+    }).then(action((response) => {
+      this.fetching = false;
+      this.response = response;
+      this.data = response.data;
+      this.error = null;
+      return response.data;
+    })).catch(action((error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.clear()
+        browserHistory.push('/')
+      }
+      this.fetching = false;
+      this.response = error.response || error;
+      this.error = get(error, 'response.data.message') || 'An error occurred. Please try again later.'
+    }));
+  })
+}
+
+export default FetchService;
---
+++ src/core/services/FormService.js
@@ -0,0 +1,87 @@
+import { extendObservable, computed, action, toJS } from 'mobx'
+import find from 'lodash/find'
+import get from 'lodash/get'
+import set from 'lodash/set'
+
+class FormService {
+  constructor() {
+    extendObservable(this, {
+      values: {},
+      dirtyValues: [],
+      validations: {},
+      // TODO: improve readability
+      errors: computed(() => {
+        const errors = Object.keys(this.validations).reduce((result, path) => {
+          if (!this.isDirty(path) && !this.submitted) {
+            return result
+          }
+          const validations = get(this.validations, path).map(validation => validation(this.getValue(path), this.getValues()))
+          const error = find(validations, error => error)
+          if (!error) {
+            return result
+          }
+          return {
+            ...result,
+            [path]: error
+          }
+        }, {})
+        if (!Object.keys(errors).length) {
+          return null
+        }
+        return errors
+      }),
+      submitted: false
+    })
+  }
+
+  getValues = () => {
+    return toJS(this.values)
+  }
+
+  getValue = path => {
+    return get(this.values, path, '')
+  }
+
+  isDirty = path => {
+    return path ? this.dirtyValues.filter(dirtyValue => dirtyValue === path).length > 0 : this.dirtyValues.length > 0
+  }
+
+  setInitialValues = action(values => {
+    this.values = values
+  })
+
+  setValue = action((path, value) => {
+    const values = {...this.values }
+    const newValues = set(values, path, value)
+    this.values = newValues
+    this.setDirty(path)
+  })
+
+  setDirty = action(path => {
+    if (this.isDirty(path)) {
+      return
+    }
+    const nextDirtyValues = [...this.dirtyValues, path]
+    this.dirtyValues = nextDirtyValues
+  })
+
+  setPristine = action(path => {
+    if (!this.isDirty(path)) {
+      return
+    }
+    const nextDirtyValues = this.dirtyValues.filter(dirtyValue => dirtyValue !== path)
+    this.dirtyValues = nextDirtyValues
+  })
+
+  setSubmitted = action(() => {
+    this.submitted = true
+  })
+
+  reset = action(() => {
+    this.values = {}
+    this.dirtyValues = []
+    this.submitted = false
+  })
+}
+
+export default FormService
