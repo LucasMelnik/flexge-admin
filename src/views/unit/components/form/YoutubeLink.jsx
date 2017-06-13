d1ce705cf5599519ce024470655181b64043e92f
Vinicius Ramos Kawamoto <vinirk@gmail.com>
2017-06-13 11:36:34 -0300

    Translation component

--- src/views/unit/components/form/YoutubeLink.jsx
+++ src/views/unit/components/form/YoutubeLink.jsx
@@ -1,50 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
-import Paper from '../../../../core/layout/Paper';
 import TextInput from '../../../../core/form/TextInput';
 
 const YoutubeLink = props => (
-  <Paper>
-      <div className="row">
-        <div className="col-lg-8">
-          <TextInput
-            floatingLabel
-            fullWidth
-            label="Youtube link"
-            value={get(props.values, 'link', '')}
-            onChange={value => props.onChange('link', value)}
-            error={get(props.errors, 'link', '')}
-          />
-        </div>
-        <div className="col-lg-2">
-          <TextInput
-            floatingLabel
-            fullWidth
-            label="Start time"
-            value={get(props.values, 'startTime', '')}
-            onChange={value => props.onChange('startTime', value)}
-            error={get(props.errors, 'startTime', '')}
-          />
-        </div>
-        <div className="col-lg-2">
-          <TextInput
-            floatingLabel
-            fullWidth
-            label="End time"
-            value={get(props.values, 'endTime', '')}
-            onChange={value => props.onChange('endTime', value)}
-            error={get(props.errors, 'endTime', '')}
-          />
-        </div>
-      </div>
-  </Paper>
+  <div className="row">
+    <div className="col-lg-8">
+      <TextInput
+        floatingLabel
+        fullWidth
+        label="Youtube link"
+        value={get(props.values, 'link', '')}
+        onChange={value => props.onChange('link', value)}
+        error={get(props.errors, 'link', '')}
+      />
+    </div>
+    <div className="col-lg-2">
+      <TextInput
+        floatingLabel
+        fullWidth
+        label="Start time"
+        value={get(props.values, 'startTime', '')}
+        onChange={value => props.onChange('startTime', value)}
+        error={get(props.errors, 'startTime', '')}
+      />
+    </div>
+    <div className="col-lg-2">
+      <TextInput
+        floatingLabel
+        fullWidth
+        label="End time"
+        value={get(props.values, 'endTime', '')}
+        onChange={value => props.onChange('endTime', value)}
+        error={get(props.errors, 'endTime', '')}
+      />
+    </div>
+  </div>
 );
 
 YoutubeLink.propTypes = {
-  link: PropTypes.string.isRequired,
-  startTime: PropTypes.string,
-  endtime: PropTypes.string,
+  values: PropTypes.object,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object,
+};
+
+YoutubeLink.defaultProps = {
+  values: {},
+  errors: {},
+  isDirty: () => false,
+  onReset: () => false,
+  onChange: () => false,
 };
 
 export default YoutubeLink;
