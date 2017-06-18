c19688c0df909737fce47516a00084d8518b04d4
Juciel de Freitas <freitas.juciel@gmail.com>
2017-06-18 13:45:41 -0300

    Fix Text Form

--- src/views/item/components/forms/TextItemForm.jsx
+++ src/views/item/components/forms/TextItemForm.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import get from 'lodash/get';
+import Row from "../../../../core/layout/Row";
+import Column from "../../../../core/layout/Column";
+import TextInput from "../../../../core/form/TextInput";
 
 const TextItemForm = props => (
   <div>
-    todo add text area
+    <Row>
+      <Column lgSize={12}>
+        <TextInput
+          multiLine
+          floatingLabel
+          fullWidth
+          label="Text"
+          disabled={props.submitting}
+          value={get(props.values, 'text', '')}
+          onChange={value => props.onChange('text', value)}
+          errorText={get(props.errors, 'text', '')}
+        />
+      </Column>
+    </Row>
   </div>
 );
 
