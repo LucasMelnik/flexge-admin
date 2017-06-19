d67e96ed0c3134742ae6dfbc508e895acd7ab35b
BRQ\jucielfreitas <jucielfreitas@brq.com>
2017-06-19 11:03:44 -0300

    Added label to explain required title

--- src/views/item/components/forms/PresentationItemForm.jsx
+++ src/views/item/components/forms/PresentationItemForm.jsx
@@ -13,7 +13,7 @@ const PresentationItemForm = props => (
         <TextInput
           floatingLabel
           fullWidth
-          label="Title"
+          label="Title (This field is required for units of type Vocabulary)"
           disabled={props.submitting}
           value={get(props.values, 'title', '')}
           onChange={value => props.onChange('title', value)}
