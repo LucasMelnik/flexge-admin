3e7be474a6ed110e01fb74dd18805ec399667392
Juciel de Freitas <freitas.juciel@gmail.com>
2017-06-20 00:19:37 -0300

    Fix single choice condition to show

--- src/views/item/components/ItemForm.jsx
+++ src/views/item/components/ItemForm.jsx
@@ -92,7 +92,7 @@ const ItemForm = props => (
             submitting={props.submitting}
           />
         )}
-        {'SINGLE_CHOICE_TEXT_SINGLE_CHOICE_AUDIO_SINGLE_CHOICE_VIDEO'.indexOf(get(props.values, 'type.key')) > -1 && (
+        {['SINGLE_CHOICE_TEXT', 'SINGLE_CHOICE_AUDIO', 'SINGLE_CHOICE_VIDEO'].find(type => type === get(props.values, 'type.key')) && (
           <SingleChoiceItemForm
             onChange={props.onChange}
             errors={props.errors}
