7cc496bea1c5758f27b7f30846945b9c522fa402
Vinicius Ramos Kawamoto <vinirk@gmail.com>
2017-06-30 00:06:56 -0300

    Removed unused imports

--- src/views/module/components/ModuleListScene.jsx
+++ src/views/module/components/ModuleListScene.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
-import {
-  Step,
-  Stepper,
-  StepLabel,
-} from 'material-ui/Stepper';
 import InlineBlock from 'jsxstyle/InlineBlock';
-import ArrowForwardIcon from 'material-ui/svg-icons/navigation/arrow-forward';
 import Separator from '../../../core/layout/Separator';
 import Button from '../../../core/form/Button';
 import ModuleListContainer from './ModuleListContainer';
@@ -14,7 +8,6 @@ import ModuleListContainer from './ModuleListContainer';
 const ModuleListScene = () => (
   <div>
     <InlineBlock height={70} />
-
     <InlineBlock
       float="right"
       marginTop={15}
