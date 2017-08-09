f24100d9725dcf692d2ed18f6c4f1a1a73c666f1
schuchowsky <rafael.arenas352@gmail.com>
2017-08-09 20:03:05 -0300

    CompanyFormScene refactor

---
+++ src/v2/core/services/FetchService.js
@@ -0,0 +1,49 @@
+import { extendObservable, action } from 'mobx';
+import { browserHistory } from 'react-router';
+import get from 'lodash/get';
+import axios from 'axios';
+import qs from 'qs';
+
+class FetchService {
+  constructor() {
+    extendObservable(this, {
+      fetching: false,
+      response: null,
+      data: null,
+      error: null,
+    });
+  }
+
+  fetch = action((params) => {
+    this.data = null;
+    this.fetching = true;
+    return axios.request({
+      method: params.method || 'get',
+      url: `${process.env.REACT_APP_API_URL}${params.url}`,
+      params: params.query,
+      paramsSerializer: params => qs.stringify(params, { arrayFormat: 'brackets' }),
+      data: params.body,
+      headers: {
+        ...params.headers,
+        ...localStorage.accessToken && { Authorization: `Bearer ${localStorage.accessToken}` },
+      },
+      ...params.auth && { auth: params.auth },
+    }).then(action((response) => {
+      this.fetching = false;
+      this.response = response;
+      this.data = response.data;
+      this.error = null;
+      return response.data;
+    })).catch(action((error) => {
+      if (error.response && error.response.status === 401) {
+        localStorage.clear();
+        browserHistory.push('/login');
+      }
+      this.fetching = false;
+      this.response = error.response || error;
+      this.error = get(error, 'response.data.message') || 'An error occurred. Please try again later.';
+    }));
+  })
+}
+
+export default FetchService;
