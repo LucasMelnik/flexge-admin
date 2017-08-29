66d2d23017f56575a53ad1eb8069ea1b4bcedb82
schuchowsky <rafael.arenas352@gmail.com>
2017-08-29 10:20:18 -0300

    Refactored menu to load once main scene mounts

--- src/views/main/components/MainScene.jsx
+++ src/views/main/components/MainScene.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TopBar from '../../../core/layout/TopBar';
 import LeftSidebar from '../../../core/layout/LeftSidebar';
@@ -11,161 +11,263 @@ import SubMenu from '../../../core/layout/SubMenu';
 import ConfirmDialogContainer from './ConfirmDialogContainer';
 import NotificationContainer from './NotificationContainer';
 
-const MainScene = props => (
-  <div>
-    <ConfirmDialogContainer />
-    <TopBar />
-    <LeftSidebar>
-      <Menu>
-        <PermissionValidator allowedFor={['ADMIN', 'DISTRIBUTOR_MANAGER', 'COMPANY_MANAGER', 'SCHOOL_MANAGER']}>
-          <MenuSection>
-            Cadastros
-          </MenuSection>
-        </PermissionValidator>
-        <PermissionValidator allowedFor={['ADMIN']}>
-          <MenuItem title="Admin" icon="fa fa-user">
-            <SubMenu
-              items={[
-                {
-                  label: 'General Configuration',
-                  link: '/configs',
-                },
-              ]}
-            />
-          </MenuItem>
-        </PermissionValidator>
-        <PermissionValidator allowedFor={['ADMIN']}>
-          <MenuItem title="Partners" icon="fa fa-users">
-            <SubMenu
-              items={[
-                {
-                  label: 'Distributors',
-                  link: '/distributors',
-                },
-                {
-                  label: 'Companies',
-                  link: '/companies',
-                },
-                {
-                  label: 'Schools',
-                  link: '/schools',
-                },
-              ]}
-            />
-          </MenuItem>
-        </PermissionValidator>
-        <PermissionValidator allowedFor={['ADMIN', 'DISTRIBUTOR_MANAGER', 'COMPANY_MANAGER', 'SCHOOL_MANAGER']}>
-          <MenuItem title="School" icon="fa fa-graduation-cap">
-            <SubMenu
-              items={[
-                {
-                  label: 'Classes',
-                  link: '/classes',
-                },
-                {
-                  label: 'Students',
-                  link: '/students',
-                },
-              ]}
-            />
-          </MenuItem>
-        </PermissionValidator>
-        <PermissionValidator allowedFor={['ADMIN']}>
-          <MenuItem title="Users" icon="fa fa-user">
-            <SubMenu
-              items={[
-                {
-                  label: 'Company Users',
-                  link: '/users',
-                },
-                {
-                  label: 'Admin Users',
-                  link: '/admin-users',
-                },
-                {
-                  label: 'Distributor Users',
-                  link: '/distributor-users',
-                },
-              ]}
-            />
-          </MenuItem>
-        </PermissionValidator>
-        <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
-          <div>
-            <MenuSection>
-              Conteúdo
-            </MenuSection>
-            <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
-              <MenuItem
-                title="Modules"
-                icon="fa fa-book"
-                link="/modules"
-              />
+class MainScene extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  componentDidMount() {
+    const $ = window.$;
+    var width = window.innerWidth;
+    if ($('body').hasClass('chat-open') || $('body').hasClass('sidebar-collapse')) {
+      this.mainmenuCollapsed();
+    } else if (width <= 767) {
+      // small window
+      $('.page-topbar').addClass('sidebar_shift').removeClass('chat_shift');
+      $('.page-sidebar').addClass('collapseit').removeClass('expandit');
+      $('body').removeClass('menuexpanded');
+      $('#main-content').addClass('sidebar_shift').removeClass('chat_shift');
+      $('.page-chatapi').removeClass('showit').addClass('hideit');
+      $('.chatapi-windows').removeClass('showit').addClass('hideit');
+      this.mainmenuCollapsed();
+    } else {
+      // large window
+      $('.page-topbar').removeClass('sidebar_shift chat_shift');
+      $('.page-sidebar').removeClass('collapseit chat_shift');
+      $('#main-content').removeClass('sidebar_shift chat_shift');
+      this.mainmenuScroll();
+    }
+
+    $('#main-menu-wrapper li a').click(function(e) {
+
+        if ($(this).next().hasClass('sub-menu') === false) {
+            return;
+        }
+
+        var parent = $(this).parent().parent();
+        var sub = $(this).next();
+
+        parent.children('li.open').children('.sub-menu').slideUp(200);
+        parent.children('li.open').children('a').children('.arrow').removeClass('open');
+        parent.children('li').removeClass('open');
+
+        if (sub.is(":visible")) {
+            $(this).find(".arrow").removeClass("open");
+            sub.slideUp(200);
+        } else {
+            $(this).parent().addClass("open");
+            $(this).find(".arrow").addClass("open");
+            sub.slideDown(200);
+        }
+
+    });
+
+    $("body").click(function(e) {
+        $(".page-sidebar.collapseit .wraplist li.open .sub-menu").attr("style","");
+        $(".page-sidebar.collapseit .wraplist li.open").removeClass("open");
+        $(".page-sidebar.chat_shift .wraplist li.open .sub-menu").attr("style","");
+        $(".page-sidebar.chat_shift .wraplist li.open").removeClass("open");
+    });
+  }
+
+  mainmenuCollapsed = () => {
+    const $ = window.$;
+    if ($('.page-sidebar.chat_shift #main-menu-wrapper').length > 0 || $('.page-sidebar.collapseit #main-menu-wrapper').length > 0) {
+      //console.log('collapse menu');
+      var topbar = $('.page-topbar').height();
+      var windowheight = window.innerHeight;
+      var minheight = windowheight - topbar;
+      var fullheight = $('.page-container #main-content .wrapper').height();
+
+      var height = fullheight;
+
+      if (fullheight < minheight) {
+          height = minheight;
+      }
+
+      $('.fixedscroll #main-menu-wrapper').perfectScrollbar('destroy');
+
+      $('.page-sidebar.chat_shift #main-menu-wrapper .wraplist, .page-sidebar.collapseit #main-menu-wrapper .wraplist').height(height);
+
+      /*hide sub menu of open menu item*/
+      $('li.open .sub-menu').attr('style', '');
+    }
+  }
+
+  mainmenuScroll = () => {
+    const $ = window.$;
+    //console.log("expand scroll menu");
+
+    var topbar = $(".page-topbar").height();
+    var projectinfo = $(".project-info").innerHeight();
+
+    var height = window.innerHeight - topbar - projectinfo;
+
+    $('.fixedscroll #main-menu-wrapper').height(height).perfectScrollbar({
+        suppressScrollX: true
+    });
+    $("#main-menu-wrapper .wraplist").height('auto');
+
+
+    /*show first sub menu of open menu item only - opened after closed*/
+    // > in the selector is used to select only immediate elements and not the inner nested elements.
+    $("li.open > .sub-menu").attr("style", "display:block;");
+  }
+
+  render() {
+    return (
+      <div>
+        <ConfirmDialogContainer />
+        <TopBar />
+        <LeftSidebar>
+          <Menu>
+            <PermissionValidator allowedFor={['ADMIN', 'DISTRIBUTOR_MANAGER', 'COMPANY_MANAGER', 'SCHOOL_MANAGER']}>
+              <MenuSection>
+                Cadastros
+              </MenuSection>
             </PermissionValidator>
-            <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
-              <MenuItem
-                title="Review"
-                icon="fa fa-eye"
-                link="/reviews"
-              />
+            <PermissionValidator allowedFor={['ADMIN']}>
+              <MenuItem title="Admin" icon="fa fa-user">
+                <SubMenu
+                  items={[
+                    {
+                      label: 'General Configuration',
+                      link: '/configs',
+                    },
+                  ]}
+                />
+              </MenuItem>
             </PermissionValidator>
-            <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
-              <MenuItem
-                title="Placement Test"
-                icon="fa fa-pencil-square"
-                link="/placement-test"
-              />
+            <PermissionValidator allowedFor={['ADMIN']}>
+              <MenuItem title="Partners" icon="fa fa-users">
+                <SubMenu
+                  items={[
+                    {
+                      label: 'Distributors',
+                      link: '/distributors',
+                    },
+                    {
+                      label: 'Companies',
+                      link: '/companies',
+                    },
+                    {
+                      label: 'Schools',
+                      link: '/schools',
+                    },
+                  ]}
+                />
+              </MenuItem>
             </PermissionValidator>
-          </div>
-        </PermissionValidator>
-        <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
-          <div>
-            <MenuSection>
-              Academic
-            </MenuSection>
-            <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
-              <MenuItem
-                title="Reports"
-                icon="fa fa-book"
-                link="/reports"
-              />
-            </PermissionValidator>
-            <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
-              <MenuItem title="Records" icon="fa fa-book">
+            <PermissionValidator allowedFor={['ADMIN', 'DISTRIBUTOR_MANAGER', 'COMPANY_MANAGER', 'SCHOOL_MANAGER']}>
+              <MenuItem title="School" icon="fa fa-graduation-cap">
                 <SubMenu
                   items={[
                     {
                       label: 'Classes',
-                      link: '/classes-academic',
+                      link: '/classes',
                     },
                     {
                       label: 'Students',
-                      link: '/students-academic',
+                      link: '/students',
+                    },
+                  ]}
+                />
+              </MenuItem>
+            </PermissionValidator>
+            <PermissionValidator allowedFor={['ADMIN']}>
+              <MenuItem title="Users" icon="fa fa-user">
+                <SubMenu
+                  items={[
+                    {
+                      label: 'Company Users',
+                      link: '/users',
+                    },
+                    {
+                      label: 'Admin Users',
+                      link: '/admin-users',
+                    },
+                    {
+                      label: 'Distributor Users',
+                      link: '/distributor-users',
                     },
                   ]}
                 />
               </MenuItem>
             </PermissionValidator>
             <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
-              <MenuItem
-                title="Grades"
-                icon="fa fa-book"
-                link="/grades"
-              />
+              <div>
+                <MenuSection>
+                  Conteúdo
+                </MenuSection>
+                <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
+                  <MenuItem
+                    title="Modules"
+                    icon="fa fa-book"
+                    link="/modules"
+                  />
+                </PermissionValidator>
+                <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
+                  <MenuItem
+                    title="Review"
+                    icon="fa fa-eye"
+                    link="/reviews"
+                  />
+                </PermissionValidator>
+                <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
+                  <MenuItem
+                    title="Placement Test"
+                    icon="fa fa-pencil-square"
+                    link="/placement-test"
+                  />
+                </PermissionValidator>
+              </div>
             </PermissionValidator>
-          </div>
-        </PermissionValidator>
-      </Menu>
-    </LeftSidebar>
-    <MainContent>
-      {props.children}
-    </MainContent>
-    <NotificationContainer />
-  </div>
-);
-
-MainScene.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+            <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
+              <div>
+                <MenuSection>
+                  Academic
+                </MenuSection>
+                <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
+                  <MenuItem
+                    title="Reports"
+                    icon="fa fa-book"
+                    link="/reports"
+                  />
+                </PermissionValidator>
+                <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
+                  <MenuItem title="Records" icon="fa fa-book">
+                    <SubMenu
+                      items={[
+                        {
+                          label: 'Classes',
+                          link: '/classes-academic',
+                        },
+                        {
+                          label: 'Students',
+                          link: '/students-academic',
+                        },
+                      ]}
+                    />
+                  </MenuItem>
+                </PermissionValidator>
+                <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN']}>
+                  <MenuItem
+                    title="Grades"
+                    icon="fa fa-book"
+                    link="/grades"
+                  />
+                </PermissionValidator>
+              </div>
+            </PermissionValidator>
+          </Menu>
+        </LeftSidebar>
+        <MainContent>
+          {this.props.children}
+        </MainContent>
+        <NotificationContainer />
+      </div>
+    );
+  }
+}
 
 export default MainScene;
