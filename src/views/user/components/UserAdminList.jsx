7fbdd97501a48d0c090a54bad910d412c6fd4cd2
Juciel de Freitas <freitas.juciel@gmail.com>
2017-08-31 16:35:06 -0300

    User routes

--- src/views/user/components/UserAdminList.jsx
+++ src/views/user/components/UserAdminList.jsx
@@ -42,7 +42,7 @@ const UserAdminList = props => (
                 {' '}
                 <IconButton
                   icon="fa-edit"
-                  onClick={() => browserHistory.push(`/companies/${row.company.id}/users/${row.id}`)}
+                  onClick={() => browserHistory.push(`/admin-users/${row.id}`)}
                 />
               </div>
             );
--- src/views/user/components/UserAdminListScene.jsx
+++ src/views/user/components/UserAdminListScene.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 import Breadcrumb from '../../../core/layout/Breadcrumb';
 import Card from '../../../core/layout/Card';
 import UserAdminListFilterContainer from './UserAdminListFilterContainer';
 import UserAdminListContainer from './UserAdminListContainer';
+import Button from '../../../core/form/Button';
 
 const UserAdminListScene = () => (
   <div>
@@ -13,7 +15,16 @@ const UserAdminListScene = () => (
         },
       ]}
     />
-    <Card title="Users">
+    <Card
+      title="Users"
+      actions={
+        <Button
+          label="New User"
+          icon="fa-plus"
+          onClick={() => browserHistory.push('/admin-users/new')}
+        />
+      }
+    >
       <UserAdminListFilterContainer />
       <UserAdminListContainer />
     </Card>
