e199b2d51602cde12ec859d14717d669a24c693e
schuchowsky <rafael.arenas352@gmail.com>
2017-09-17 10:41:19 -0300

    Add Level and Order to Student Placement Results

--- src/views/student/components/placement/StudentPlacementListContainer.jsx
+++ src/views/student/components/placement/StudentPlacementListContainer.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { toJS } from 'mobx';
 import { observer } from 'mobx-react';
+import find from 'lodash/find';
 import StudentPlacementListService from '../../services/StudentPlacementListService';
+import PlacementTestListService from '../../../placement-test/services/PlacementTestListService';
 import StudentPlacementList from './StudentPlacementList';
 
 class StudentPlacementListContainer extends Component {
@@ -13,12 +15,35 @@ class StudentPlacementListContainer extends Component {
 
   componentDidMount() {
     StudentPlacementListService.init(this.props.studentId);
+    PlacementTestListService.load();
   }
 
   render() {
+    let placements;
+    if (StudentPlacementListService.fetch.fetching || PlacementTestListService.fetch.fetching) {
+      placements = [];
+    } else {
+      placements = toJS(StudentPlacementListService.placements.map((placement) => {
+        const ptWithOrderAndLevel = {
+          ...placement,
+          answeredItems: placement.answeredItems.map((item) => {
+            const grammarPlacementTestLevel = find(
+              PlacementTestListService.placementTests,
+              pt => find(pt.items, itemId => itemId === item.item),
+            );
+            return {
+              ...item,
+              order: grammarPlacementTestLevel ? grammarPlacementTestLevel.order : 0,
+              level: grammarPlacementTestLevel ? grammarPlacementTestLevel.placementTestLevel.level : 0,
+            };
+          }),
+        };
+        return ptWithOrderAndLevel;
+      }));
+    }
     return (
       <StudentPlacementList
-        placements={toJS(StudentPlacementListService.placements)}
+        placements={placements}
         fetching={StudentPlacementListService.fetch.fetching}
         onDelete={StudentPlacementListService.handleRemove}
       />
