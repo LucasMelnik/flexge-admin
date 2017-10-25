2e31d906f7a06e0ef3f45df371f68700d3a8e702
Vinicius Ramos Kawamoto <vinirk@gmail.com>
2017-10-25 15:50:14 -0200

    Fix report student content

--- src/views/reports/components/student-detail/StudentDetailContentRecordList.jsx
+++ src/views/reports/components/student-detail/StudentDetailContentRecordList.jsx
@@ -11,22 +11,54 @@ const StudentDetailContentRecordList = props => (
         label: 'Name',
         path: 'name',
         sort: true,
+        render: (value, row) => (row.name ? row.name : moment(row.startedAt).format('DD/MM/YYYY HH:mm')),
       },
       {
         label: 'Time',
-        path: 'time',
+        render: (value, row) => {
+          if (row.startedAt && row.completedAt) {
+            const duration = moment.duration(moment(row.completedAt).diff(moment(row.startedAt)));
+            return duration.format('mm:ss');
+          }
+        },
       },
       {
-        label: 'Complete',
+        label: 'Score',
+        path: 'score',
       },
       {
-        label: 'Pontos',
+        label: 'Correct',
+        render: (value, row) => {
+          if (row.unititemresults) {
+            let wrong = 0;
+            row.unititemresults.forEach((item) => {
+              if (!item.correct) {
+                wrong += 1;
+              }
+            });
+            return `${row.unititemresults.length - wrong}/${row.unititemresults.length}`;
+          }
+        },
       },
       {
-        label: 'Corretas',
+        label: 'Read count',
+        path: 'readCount',
       },
       {
-        label: 'Nível',
+        label: 'Record count',
+        path: 'recordCount',
+      },
+      {
+        label: 'Repeat count',
+        path: 'repeatCount',
+      },
+      {
+        label: 'Listen count',
+        path: 'listenCount',
+      },
+      {
+        label: 'Translate count',
+        path: 'translateCount',
       },
     ]}
     dataSource={props.contents}
