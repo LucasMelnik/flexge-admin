0af2888ea4a5edceab5d98ba557d41610ed021fb
Juciel de Freitas <freitas.juciel@gmail.com>
2017-11-17 14:47:33 -0200

    Added form to move unit to other course and module

--- src/views/module/components/unit/UnitFormScene.jsx
+++ src/views/module/components/unit/UnitFormScene.jsx
@@ -5,6 +5,8 @@ import UnitFormContainer from './UnitFormContainer';
 import Button from '../../../../core/form/Button';
 import Card from '../../../../core/layout/Card';
 import Breadcrumb from '../../../../core/layout/Breadcrumb';
+import MoveUnitFormContainer from './MoveUnitFormContainer';
+import Separator from '../../../../core/layout/Separator';
 
 const UnitFormScene = props => (
   <div>
@@ -20,8 +22,8 @@ const UnitFormScene = props => (
           link: `/modules/${props.module.id}/details`,
         },
         {
-          text: props.masteryTestId ? 'Edit Mastery Test': 'New Mastery Test',
-        }
+          text: props.unitId ? 'Edit Unit' : 'New Unit',
+        },
       ]}
     />
     <Card
@@ -39,6 +41,17 @@ const UnitFormScene = props => (
         moduleId={props.moduleId}
       />
     </Card>
+    {(localStorage.role === 'ADMIN' && props.unitId) && (
+      <div>
+        <Separator />
+        <Card title="Move Unit to other Course/Module">
+          <MoveUnitFormContainer
+            unitId={props.unitId}
+            moduleId={props.moduleId}
+          />
+        </Card>
+      </div>
+    )}
   </div>
 );
 
