547c5e8b91890efd440fbb1085ff85786cee1d71
Juciel de Freitas <freitas.juciel@gmail.com>
2017-11-20 14:07:32 -0200

    Refactor main scene

--- src/views/main/components/MainScene.jsx
+++ src/views/main/components/MainScene.jsx
@@ -1,198 +1,183 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import TopBar from '../../../core/layout/TopBar';
-import MainContent from '../../../core/layout/MainContent';
+import { Layout } from 'antd';
 import Menu from '../../../core/layout/Menu';
 import MenuItem from '../../../core/layout/MenuItem';
-import PermissionValidator from '../../../core/layout/PermissionValidator';
 import SubMenu from '../../../core/layout/SubMenu';
 import ConfirmDialogContainer from './ConfirmDialogContainer';
 import NotificationContainer from './NotificationContainer';
 
-class MainScene extends Component {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-  };
+const MainScene = props => (
+  <Layout>
+    <Layout.Header>
+      <Menu>
+        <SubMenu
+          title="Admin"
+          icon="fa fa-user"
+          allowedFor={['ADMIN']}
+          items={[
+            {
+              label: 'General Configuration',
+              link: '/configuration',
+            },
+            {
+              label: 'Item types',
+              link: '/item-types',
+            },
+            {
+              label: 'Placement test level',
+              link: '/placement-test-levels',
+            },
+            {
+              label: 'Courses',
+              link: '/courses',
+            },
+            {
+              label: 'Characters',
+              link: '/characters',
+            },
+            {
+              label: 'Regions',
+              link: '/regions',
+            },
+            {
+              label: 'Achievements',
+              link: '/achievements',
+            },
+          ]}
+        />
+        <SubMenu
+          title="Partners"
+          icon="fa fa-users"
+          allowedFor={['ADMIN']}
+          items={[
+            {
+              label: 'Distributors',
+              link: '/distributors',
+            },
+            {
+              label: 'Companies',
+              link: '/companies',
+            },
+            {
+              label: 'Schools',
+              link: '/schools',
+            },
+          ]}
+        />
+        <SubMenu
+          title="Users"
+          icon="fa fa-user"
+          allowedFor={['ADMIN']}
+          items={[
+            {
+              label: 'Company Users',
+              link: '/users',
+            },
+            {
+              label: 'Admin Users',
+              link: '/admin-users',
+            },
+            {
+              label: 'Distributor Users',
+              link: '/distributor-users',
+            },
+          ]}
+        />
+        <SubMenu
+          title="School"
+          icon="fa fa-graduation-cap"
+          allowedFor={['ADMIN', 'DISTRIBUTOR_MANAGER', 'COMPANY_MANAGER', 'SCHOOL_MANAGER']}
+          items={[
+            {
+              label: 'Students',
+              link: '/students',
+            },
+          ]}
+        />
+        <MenuItem
+          title="Modules"
+          icon="fa fa-book"
+          link="/modules"
+          allowedFor={['ADMIN', 'CONTENT_ADMIN', 'IMAGE_ADMIN']}
+        />
+        <MenuItem
+          title="Review"
+          icon="fa fa-eye"
+          link="/reviews"
+          allowedFor={['ADMIN', 'CONTENT_ADMIN', 'IMAGE_ADMIN']}
+        />
+        <MenuItem
+          title="Placement Test"
+          icon="fa fa-pencil-square"
+          link="/placement-test"
+          allowedFor={['ADMIN', 'CONTENT_ADMIN', 'IMAGE_ADMIN']}
+        />
+        <MenuItem
+          title="Practice Test"
+          icon="fa fa-book"
+          link="/practice-tests"
+          allowedFor={['ADMIN', 'CONTENT_ADMIN', 'IMAGE_ADMIN']}
+        />
+        <SubMenu
+          title="Reports"
+          icon="fa fa-file-o"
+          allowedFor={['ADMIN']}
+          items={[
+            {
+              label: 'Placement Item History',
+              link: '/placement-test-items-history',
+            },
+            {
+              label: 'Student Placement/Mastery Tests',
+              link: '/student-tests',
+            },
+            {
+              label: 'Records',
+              link: '/records/schools',
+            },
+            {
+              label: 'Approved Images Records',
+              link: '/records/unit-images',
+            },
+            {
+              label: 'Unit Items Errors',
+              link: '/records/unit-errors',
+            },
+            {
+              label: 'Grades',
+              link: '/grades',
+            },
+          ]}
+        />
+        <SubMenu
+          title="Audio"
+          icon="fa fa-file-o"
+          allowedFor={['AUDIO_CONTENT', 'ADMIN']}
+          items={[
+            {
+              label: 'Upload Management',
+              link: '/item-audios',
+            },
+          ]}
+        />
+      </Menu>
+    </Layout.Header>
+    <Layout.Content
+      style={{
+        padding: 10,
+        height: '100vh',
+      }}
+    >
+      {props.children}
+      <ConfirmDialogContainer />
+      <NotificationContainer />
+    </Layout.Content>
+  </Layout>
+);
 
-  componentDidMount() {
-    const $ = window.$;
-    $('.page-topbar li.profile').addClass('showopacity');
-  }
-
-  render() {
-    return (
-      <div>
-        <ConfirmDialogContainer />
-        <TopBar />
-        <Menu>
-          <PermissionValidator allowedFor={['ADMIN']}>
-            <MenuItem title="Admin" icon="fa fa-user">
-              <SubMenu
-                items={[
-                  {
-                    label: 'General Configuration',
-                    link: '/configuration',
-                  },
-                  {
-                    label: 'Item types',
-                    link: '/item-types',
-                  },
-                  {
-                    label: 'Placement test level',
-                    link: '/placement-test-levels',
-                  },
-                  {
-                    label: 'Courses',
-                    link: '/courses',
-                  },
-                  {
-                    label: 'Characters',
-                    link: '/characters',
-                  },
-                  {
-                    label: 'Regions',
-                    link: '/regions',
-                  },
-                  {
-                    label: 'Achievements',
-                    link: '/achievements',
-                  },
-                ]}
-              />
-            </MenuItem>
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['ADMIN']}>
-            <MenuItem title="Partners" icon="fa fa-users">
-              <SubMenu
-                items={[
-                  {
-                    label: 'Distributors',
-                    link: '/distributors',
-                  },
-                  {
-                    label: 'Companies',
-                    link: '/companies',
-                  },
-                  {
-                    label: 'Schools',
-                    link: '/schools',
-                  },
-                ]}
-              />
-            </MenuItem>
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['ADMIN', 'DISTRIBUTOR_MANAGER', 'COMPANY_MANAGER', 'SCHOOL_MANAGER']}>
-            <MenuItem title="School" icon="fa fa-graduation-cap">
-              <SubMenu
-                items={[
-                  {
-                    label: 'Students',
-                    link: '/students',
-                  },
-                ]}
-              />
-            </MenuItem>
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['ADMIN']}>
-            <MenuItem title="Users" icon="fa fa-user">
-              <SubMenu
-                items={[
-                  {
-                    label: 'Company Users',
-                    link: '/users',
-                  },
-                  {
-                    label: 'Admin Users',
-                    link: '/admin-users',
-                  },
-                  {
-                    label: 'Distributor Users',
-                    link: '/distributor-users',
-                  },
-                ]}
-              />
-            </MenuItem>
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN', 'IMAGE_ADMIN']}>
-            <MenuItem
-              title="Modules"
-              icon="fa fa-book"
-              link="/modules"
-            />
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN', 'IMAGE_ADMIN']}>
-            <MenuItem
-              title="Review"
-              icon="fa fa-eye"
-              link="/reviews"
-            />
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN', 'IMAGE_ADMIN']}>
-            <MenuItem
-              title="Placement Test"
-              icon="fa fa-pencil-square"
-              link="/placement-test"
-            />
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['ADMIN', 'CONTENT_ADMIN', 'IMAGE_ADMIN']}>
-            <MenuItem
-              title="Practice Test"
-              icon="fa fa-book"
-              link="/practice-tests"
-            />
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['ADMIN']}>
-            <MenuItem title="Reports" icon="fa fa-file-o">
-              <SubMenu
-                items={[
-                  {
-                    label: 'Placement Item History',
-                    link: '/placement-test-items-history',
-                  },
-                  {
-                    label: 'Student Placement/Mastery Tests',
-                    link: '/student-tests',
-                  },
-                  {
-                    label: 'Records',
-                    link: '/records/schools',
-                  },
-                  {
-                    label: 'Approved Images Records',
-                    link: '/records/unit-images',
-                  },
-                  {
-                    label: 'Unit Items Errors',
-                    link: '/records/unit-errors',
-                  },
-                  {
-                    label: 'Grades',
-                    link: '/grades',
-                  },
-                ]}
-              />
-            </MenuItem>
-          </PermissionValidator>
-          <PermissionValidator allowedFor={['AUDIO_CONTENT', 'ADMIN']}>
-            <MenuItem title="Audio" icon="fa fa-file-o">
-              <SubMenu
-                items={[
-                  {
-                    label: 'Upload Management',
-                    link: '/item-audios',
-                  },
-                ]}
-              />
-            </MenuItem>
-          </PermissionValidator>
-        </Menu>
-        <MainContent>
-          {this.props.children}
-        </MainContent>
-        <NotificationContainer />
-      </div>
-    );
-  }
-}
+MainScene.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default MainScene;
