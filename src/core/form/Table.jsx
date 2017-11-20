6d37279a1b8fb5d0be988e05a7a308ec8e72ca2d
Juciel de Freitas <freitas.juciel@gmail.com>
2017-11-20 14:05:53 -0200

    initial refactor table

--- src/core/form/Table.jsx
+++ src/core/form/Table.jsx
@@ -1,137 +1,64 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
-import orderBy from 'lodash/orderBy';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
-import Icon from '../layout/Icon';
+import isString from 'lodash/isString';
+import { Table as AntTable } from 'antd';
 
-export default class Table extends Component {
-
-  static propTypes = {
-    columns: PropTypes.arrayOf(PropTypes.shape({
-      label: PropTypes.string.isRequired,
-      path: PropTypes.string,
-      isKey: PropTypes.bool,
-      hidden: PropTypes.bool,
-      render: PropTypes.func,
-      width: PropTypes.string,
-      rowColumnStyle: PropTypes.object,
-    })),
-    rows: PropTypes.arrayOf(PropTypes.object),
-    selectable: PropTypes.bool,
-    onSelect: PropTypes.func,
-    expandable: PropTypes.bool,
-    expandableComponent: PropTypes.func,
-  };
-
-  static defaultProps = {
-    columns: [],
-    rows: [],
-    expandable: false,
-    selectable: false,
-    onSelect: null,
-    expandableComponent: null,
-  };
-
-  state = { expandedRows: [], data: [] };
-
-  componentDidMount() {
-    this.setState({
-      data: this.props.rows,
-    });
+const sort = (a, b, path) => {
+  if (isString(get(a, path, ''))) {
+    return get(a, path, '').localeCompare(get(b, path, ''));
   }
+  return a[path] - b[path];
+};
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      data: nextProps.rows,
-    });
-  }
+const Table = props => (
+  <AntTable
+    pagination={props.pagination || false}
+    size="middle"
+    rowKey="id"
+    bordered
+    locale={{
+      filterConfirm: 'Ok',
+      filterReset: 'Reset',
+      emptyText: 'No Data',
+    }}
+    dataSource={props.rows}
+    columns={props.columns.map(column => ({
+      title: column.label,
+      width: column.width,
+      dataIndex: column.path,
+      render: column.render,
+      sorter: column.sort ? (a, b) => sort(a, b, column.path) : null,
+    }))}
+    onChange={props.onChange}
+    onRowClick={row => props.selectable && props.onSelect(row)}
+    loading={props.fetching}
+    expandedRowRender={props.expandableComponent}
+  />
+);
 
-  handleSort = (path, order) => {
-    this.setState({
-      data: orderBy(this.state.data, path, order),
-    });
-  };
+Table.propTypes = {
+  rows: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  columns: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  pagination: PropTypes.shape({
+    current: PropTypes.number,
+    total: PropTypes.number,
+    pageSize: PropTypes.number,
+  }),
+  onSelect: PropTypes.func,
+  onChange: PropTypes.func,
+  fetching: PropTypes.bool,
+  selectable: PropTypes.bool,
+  expandableComponent: PropTypes.func,
+};
 
-  renderColumnValue = (cell, row, path) => {
-    return get(row, path, '');
-  };
+Table.defaultProps = {
+  onSelect: null,
+  onChange: null,
+  fetching: false,
+  pagination: null,
+  selectable: false,
+  expandableComponent: null,
+};
 
-  renderExpandableIcon = ({ isExpanded }) => {
-    return <Icon name={isExpanded ? 'fa-chevron-down' : 'fa-chevron-right'} />
-  };
-
-  render() {
-    return (
-      <div>
-        <BootstrapTable
-          data={this.state.data}
-          striped
-          hover
-          search={false}
-          remote
-          options={{
-            expandBy: 'column',
-            onSortChange: this.handleSort,
-          }}
-          expandableRow={() => this.props.expandable}
-          expandComponent={(row) => {
-            if (this.state.expandedRows.find(id => id === row.id)) {
-              return this.props.expandableComponent(row);
-            } else {
-              return null;
-            }
-          }}
-          expandColumnOptions={{
-            expandColumnVisible: this.props.expandable,
-            expandColumnComponent: this.renderExpandableIcon,
-            columnWidth: 40,
-          }}
-          selectRow={{
-            mode: 'checkbox',
-            hideSelectColumn: true,
-            clickToSelect: true,
-            clickToExpand: true,
-            onSelect: (row, isSelected, e) => {
-              if (this.props.expandable) {
-                this.setState({
-                  expandedRows: [
-                    ...this.state.expandedRows,
-                    row.id,
-                  ],
-                });
-              }
-
-              if (window.$(e.target).is('button') || window.$(e.target).is('i') || window.$(e.target).is('span')) {
-                return false;
-              }
-              if (this.props.selectable && this.props.onSelect) {
-                this.props.onSelect(row);
-              }
-            },
-          }}
-          trClassName={this.props.selectable && 'selectable-row'}
-        >
-          {this.props.columns.map(column => (
-            <TableHeaderColumn
-              key={`column-definition-${column.label}`}
-              isKey={column.isKey}
-              hidden={column.hidden}
-              dataField={column.path}
-              dataSort={column.path !== 'action'}
-              dataFormat={column.render || this.renderColumnValue}
-              formatExtraData={column.path}
-              tdStyle={column.rowColumnStyle}
-              width={column.width || 'auto'}
-              expandable={false}
-            >
-              {column.label}
-            </TableHeaderColumn>
-          ))}
-        </BootstrapTable>
-        <small>{this.props.rows.length} record{this.props.rows.length > 1 && 's'} found.</small>
-      </div>
-    );
-  }
-}
+export default Table;
