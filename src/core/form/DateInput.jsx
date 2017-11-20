aca0d4fa2c500c33de7eabc94b56b2d2451c4f03
Juciel de Freitas <freitas.juciel@gmail.com>
2017-11-20 14:30:44 -0200

    Added form item to date picker

--- src/core/form/DateInput.jsx
+++ src/core/form/DateInput.jsx
@@ -1,32 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import { DatePicker } from 'antd';
+import { DatePicker, Form } from 'antd';
 
 const DateInput = props =>  (
-  <DatePicker
-    value={moment(props.value)}
-    onChange={props.onChange}
-    format={props.format}
-    placeholder={props.placeholder}
-    disabled={props.disabled}
-  />
+  <Form.Item
+    label={props.label}
+    help={props.errorText}
+    validateStatus={props.errorText && 'error'}
+  >
+    <DatePicker
+      value={moment(props.value)}
+      onChange={props.onChange}
+      format={props.format}
+      placeholder={props.placeholder}
+      disabled={props.disabled}
+    />
+  </Form.Item>
 );
 
 DateInput.propTypes = {
   format: PropTypes.string,
+  errorText: PropTypes.string,
+  label: PropTypes.string.isRequired,
   value: PropTypes.instanceOf(Date),
   placeholder: PropTypes.string,
   disabled: PropTypes.bool,
-  onChange: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
 };
 
 DateInput.defaultProps = {
   format: 'DD/MM/YYYY',
+  errorText: null,
   value: null,
   placeholder: null,
   disabled: false,
-  onChange: () => false,
 };
 
-export default  DateInput;
+export default DateInput;
