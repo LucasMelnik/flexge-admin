f99562e0c25bce2f496ad02565a732809435f0ea
Juciel de Freitas <freitas.juciel@gmail.com>
2017-11-20 16:15:26 -0200

    Breadcrumb refactor

--- src/core/layout/Icon.jsx
+++ src/core/layout/Icon.jsx
@@ -5,11 +5,17 @@ import { Icon as AntIcon } from 'antd';
 const Icon = props => (
   <AntIcon
     type={props.name}
+    style={props.style}
   />
 );
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
+  style: PropTypes.shape({}),
+};
+
+Icon.defaultProps = {
+  style: {},
 };
 
 export default Icon;
