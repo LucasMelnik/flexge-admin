4a0a2158aeb78aaef825271fded5c55b8e7f15f7
Juciel de Freitas <freitas.juciel@gmail.com>
2017-11-29 11:15:00 -0200

    Added fake menu to teacher role

--- src/views/main/components/MainScene.jsx
+++ src/views/main/components/MainScene.jsx
@@ -229,6 +229,30 @@ const MainScene = props => (
             },
           ]}
         />
+        <SubMenu
+          title="Basic Register"
+          icon="folder"
+          allowedFor={['TEACHER']}
+          groups={[]}
+        />
+        <SubMenu
+          title="Academic"
+          icon="book"
+          allowedFor={['TEACHER']}
+          groups={[]}
+        />
+        <SubMenu
+          title="Certifications"
+          icon="trophy"
+          allowedFor={['TEACHER']}
+          groups={[]}
+        />
+        <SubMenu
+          title="Records"
+          icon="folder-open"
+          allowedFor={['TEACHER']}
+          groups={[]}
+        />
       </Menu>
       <div
         style={{
