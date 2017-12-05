6fdd2d2283d3742f3e2269be55c4c4ea9592781f
Vinicius Ramos Kawamoto <vinirk@gmail.com>
2017-12-05 16:31:08 -0200

    Change english level to line chart

--- src/views/dashboard/components/common/SchoolEnglishLevelChart.jsx
+++ src/views/dashboard/components/common/SchoolEnglishLevelChart.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import Card from '../../../../core/layout/Card';
-import BarChart from '../../../../core/chart/BarChart';
+import LineChart from '../../../../core/chart/LineChart';
 
 const SchoolEnglishLevelChart = () => (
   <Card
     title="Nível de Inglês"
   >
-    <BarChart
-      labels={['< 6 m', 'entre 6 m - 1 ano', '1 ano - 2 anos', '2 anos - 3 anos', '> 3 anos']}
-      data={[0.7, 1.3, 2.1, 3.5, 4]}
+    <LineChart
+      labels={[
+        '< 6 months',
+        '6m ~ 1y',
+        '1y6m ~ 2y',
+        '2y ~ 2y6m',
+        '2y6m ~ 3y',
+        '3y ~ 3y6m',
+        '3y6m ~ 4y',
+        '4y ~ 4y6m',
+        '4y6m ~ 5y',
+      ]}
+      xAxesLabelString="Time"
+      yAxesLabelString="Courses"
+      data={[
+        0,
+        1,
+        1.5,
+        2,
+        2.5,
+        3,
+        3.5,
+        4,
+        4.5,
+        5,
+        5.5,
+        6
+      ]}
+      dataFormat={[
+        {
+          label: 'Nível de inglês',
+          valueRender: item => item,
+        },
+      ]}
     />
   </Card>
 );
