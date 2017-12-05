0311ec1732a150df55631eadde4aac69fbcb44a7
Vinicius Ramos Kawamoto <vinirk@gmail.com>
2017-12-05 19:18:18 -0200

    Add select component to filter history dashboard

--- src/views/dashboard/components/teacher/HistoryCharts.jsx
+++ src/views/dashboard/components/teacher/HistoryCharts.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import StudentStudyHistoryChart from '../common/StudentStudyHistoryChart';
 import Separator from '../../../../core/layout/Separator';
 import StudentAverageStudiedTimeChart from '../common/StudentAverageStudiedTimeChart';
+import HistoryListFilterContainer from '../common/HistoryListFilterContainer';
 
 const HistoryCharts = () => (
   <div>
-    <h1>History By Year</h1>
+    <div
+      style={{
+        display: 'flex',
+      }}
+    >
+      <h1
+        style={{
+          marginRight: 10,
+        }}
+      >
+        History By Year
+      </h1>
+      <HistoryListFilterContainer />
+    </div>
     <Separator />
     <StudentStudyHistoryChart />
     <Separator />
