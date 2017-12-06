3c4ef33a996f754070023b539862613ea524479f
Rafael Arenas Schuchowsky <rafael.arenas352@gmail.com>
2017-12-06 08:17:36 -0200

    School list changes

--- src/views/school/components/SchoolList.jsx
+++ src/views/school/components/SchoolList.jsx
@@ -9,34 +9,33 @@ const SchoolList = props => (
     fetching={props.fetching}
     columns={[
       {
-        label: 'Name',
-        path: 'name',
-        sort: true,
-      },
-      {
         label: 'Company',
         path: 'company.name',
+        sort: true,
+        width: 250,
+      },
+      {
+        label: 'School',
+        path: 'name',
         sort: true,
       },
       {
         label: 'Actions',
         path: 'action',
         width: '85px',
-        render: (cell, row) => {
-          return (
-            <div>
-              <Button
-                icon="delete"
-                onClick={() => props.onDelete(row)}
-              />
-              {' '}
-              <Button
-                icon="edit"
-                onClick={() => browserHistory.push(`${props.baseUrl}/schools/${row.id}`)}
-              />
-            </div>
-          );
-        },
+        render: (cell, row) => (
+          <div>
+            <Button
+              icon="delete"
+              onClick={() => props.onDelete(row)}
+            />
+            {' '}
+            <Button
+              icon="edit"
+              onClick={() => browserHistory.push(`${props.baseUrl}/schools/${row.id}`)}
+            />
+          </div>
+        ),
       },
     ]}
     rows={props.schools}
