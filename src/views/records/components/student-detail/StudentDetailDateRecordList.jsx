f0a84fbe2392186180c0a4cc0bc126747d938537
Juciel de Freitas <freitas.juciel@gmail.com>
2017-12-08 16:20:12 -0200

    Minor fixes

--- src/views/records/components/student-detail/StudentDetailDateRecordList.jsx
+++ src/views/records/components/student-detail/StudentDetailDateRecordList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import round from 'lodash/round';
 import Table from '../../../../core/form/Table';
 
 const StudentDetailDateRecordList = props => (
