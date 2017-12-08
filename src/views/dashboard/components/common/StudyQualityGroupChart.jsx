0d033ec47e8de74a3a04b78f81d1c45ac95c03ff
Rafael Arenas Schuchowsky <rafael.arenas352@gmail.com>
2017-12-08 14:19:35 -0200

    Eslint warning

--- src/views/dashboard/components/common/StudyQualityGroupChart.jsx
+++ src/views/dashboard/components/common/StudyQualityGroupChart.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import reverse from 'lodash/reverse';
 import Card from '../../../../core/layout/Card';
 import DoughnutChart from '../../../../core/chart/DoughnutChart';
-import reverse from 'lodash/reverse';
 import { ORANGE, RED, GREEN, DARK_GREEN } from '../../../../core/chart/colors';
 
 const StudyQualityGroupChart = props => (
