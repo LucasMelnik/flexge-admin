a2e362a56faf20e9f0860c931615edc5c9fcfec1
Rafael Arenas Schuchowsky <rafael.arenas352@gmail.com>
2017-12-11 13:06:21 -0200

    Get studied time label from school config

--- src/views/dashboard/components/common/metrics/StudiedTimeChart.jsx
+++ src/views/dashboard/components/common/metrics/StudiedTimeChart.jsx
@@ -7,28 +7,37 @@ import Separator from '../../../../../core/layout/Separator';
 import { DARK_GREEN, GREEN, ORANGE, RED } from '../../../../../core/chart/colors';
 import TopStudentsTableContainer from './TopStudentsTableContainer';
 
-const StudiedTimeChart = props => (
-  <Card
-    title="Studied time last 7 days"
-    loading={props.loading}
-  >
-    <DoughnutChart
-      labels={['More than 2h', 'Until 2h', 'Until 1h', 'Didn\'t study']}
-      data={props.data}
-      colors={[DARK_GREEN, GREEN, ORANGE, RED]}
-    />
-    {props.showDetails && (
-      <Separator size="md" />
-    )}
-    {props.showDetails && (
-      <TopStudentsTableContainer
-        days={7}
-        from={moment().subtract(7, 'days').startOf('day').toDate()}
-        to={moment().endOf('day').toDate()}
+const StudiedTimeChart = (props) => {
+  const school = localStorage.getItem('school');
+  const weeklyHoursRequired = school ? school.weeklyHoursRequired : 2;
+  return (
+    <Card
+      title="Studied time last 7 days"
+      loading={props.loading}
+    >
+      <DoughnutChart
+        labels={[
+          `More than ${weeklyHoursRequired}h`,
+          `Until ${weeklyHoursRequired}h`,
+          'Until 1h',
+          'Didn\'t study',
+        ]}
+        data={props.data}
+        colors={[DARK_GREEN, GREEN, ORANGE, RED]}
       />
-    )}
-  </Card>
-);
+      {props.showDetails && (
+        <Separator size="md" />
+      )}
+      {props.showDetails && (
+        <TopStudentsTableContainer
+          days={7}
+          from={moment().subtract(7, 'days').startOf('day').toDate()}
+          to={moment().endOf('day').toDate()}
+        />
+      )}
+    </Card>
+  );
+}
 
 StudiedTimeChart.propTypes = {
   showDetails: PropTypes.bool,
