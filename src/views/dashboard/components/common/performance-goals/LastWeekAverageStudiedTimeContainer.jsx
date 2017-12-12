88666f73e67fb084bb273813cff5a5d6882c97ba
Rafael Arenas Schuchowsky <rafael.arenas352@gmail.com>
2017-12-12 10:17:22 -0200

    Last week average study time integration

--- src/views/dashboard/components/common/performance-goals/LastWeekAverageStudiedTimeContainer.jsx
+++ src/views/dashboard/components/common/performance-goals/LastWeekAverageStudiedTimeContainer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import moment from 'moment';
-import get from 'lodash/get';
 import 'moment-duration-format';
 import LastWeekAverageStudiedTimeService from '../../../services/LastWeekAverageStudiedTimeService';
 import CircularProgress from '../../../../../core/layout/CircularProgress';
@@ -14,14 +13,14 @@ const ActiveStudentsLastSevenDaysContainer = () => {
       noDataText="No data found"
       title="Average time last week"
       tooltip="Average studied time from Monday to Sunday last week"
-      value={get(LastWeekAverageStudiedTimeService.fetch, 'data.averageStudiedTime')}
+      value={LastWeekAverageStudiedTimeService.average}
       max={school ? school.weeklyHoursRequired : 2}
-      successCondition={value => value > school ? school.weeklyHoursRequired : 2}
-      badCondition={value => value <= 0.999}
+      successCondition={value => school ? (value > school.weeklyHoursRequired) : (value > 2)}
+      badCondition={value => value < 1}
       valueRender={value => moment.duration(value, 'hours').format('hh:mm', { trim: false })}
       legend={localStorage.role === 'TEACHER' &&
         `School average ${
-          moment.duration(get(LastWeekAverageStudiedTimeService.fetch, 'data.schoolAverageStudiedTime'), 'hours').format('hh:mm', { trim: false })}`
+          moment.duration(LastWeekAverageStudiedTimeService.schoolAverage, 'hours').format('hh:mm', { trim: false })}`
       }
     />
   );
--- src/views/dashboard/services/LastWeekAverageStudiedTimeService.js
+++ src/views/dashboard/services/LastWeekAverageStudiedTimeService.js
@@ -1,17 +1,41 @@
-import { action } from 'mobx';
+import { action, extendObservable, computed } from 'mobx';
 import FetchService from '../../../core/services/FetchService';
 
 class LastWeekAverageStudiedTimeService {
   fetch = new FetchService();
 
+  constructor() {
+    extendObservable(this, {
+      data: [],
+      average: computed(() => {
+        if (!this.data.length) return 0;
+        const total = this.data.reduce((acc, school) => (
+          acc + school.classes.reduce((classAcc, schoolClass) => (
+            acc + schoolClass.averageStudiedTime
+          ), 0)
+        ), 0);
+        console.log('total', total);
+        return total / this.data.reduce((acc, school) => acc + school.classes.length, 0);
+      }),
+      schoolAverage: computed(() => {
+        if (!this.data.length) return 0;
+        return this.data.reduce((acc, school) => (
+          acc + school.schoolAverageStudiedTime
+        ), 0) / this.data.length;
+      }),
+    });
+  }
+
   load = action((from, to) => {
-    this.data = {};
+    this.data = [];
     this.fetch.fetch({
       url: '/reports/last-week-average-studied-time',
       query: {
         from,
         to,
       },
+    }).then(() => {
+      this.data = this.fetch.data;
     });
   });
 }
