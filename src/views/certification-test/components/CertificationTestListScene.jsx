5da456ea7fd8bd83084c8bbda03b58e27ac0e6ec
Juciel de Freitas <freitas.juciel@gmail.com>
2017-12-22 15:21:16 -0200

    Fix imports

--- src/views/certification-test/components/CertificationTestListScene.jsx
+++ src/views/certification-test/components/CertificationTestListScene.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { browserHistory } from 'react-router';
 import Breadcrumb from '../../../core/layout/Breadcrumb';
 import Card from '../../../core/layout/Card';
-import Button from '../../../core/form/Button';
 import Tabs from '../../../core/layout/Tabs';
 import CertificationTestListPendingContainer from './CertificationTestListPendingContainer';
 import CertificationTestListScheduledContainer from './CertificationTestListScheduledContainer';
 import CertificationTestListCompletedContainer from './CertificationTestListCompletedContainer';
+import Button from '../../../core/form/Button';
 
 const CertificationTestListScene = () => (
   <div>
