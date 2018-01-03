f5a58186aeb10fe2d2c5b89a726d4cfba630fdd0
Juciel de Freitas <freitas.juciel@gmail.com>
2018-01-03 16:43:01 -0200

    fix course form to allow the certification test course selection

--- src/views/course/components/CourseForm.jsx
+++ src/views/course/components/CourseForm.jsx
@@ -5,6 +5,7 @@ import Row from '../../../core/layout/Row';
 import Column from '../../../core/layout/Column';
 import TextInput from '../../../core/form/TextInput';
 import FormButtons from '../../../core/form/FormButtons';
+import FetchSelect from '../../../core/form/FetchSelect';
 
 const CourseForm = props => (
   <form
@@ -22,6 +23,20 @@ const CourseForm = props => (
           value={get(props.values, 'name', '')}
           onChange={value => props.onChange('name', value)}
           errorText={get(props.errors, 'name', null)}
+        />
+      </Column>
+      <Column size={3}>
+        <FetchSelect
+          url="courses"
+          disabled={props.submitting}
+          label="Certification Test Course"
+          value={get(props.values, 'certificationTestCourse', '')}
+          onChange={course => props.onChange('certificationTestCourse', course)}
+          errorText={get(props.errors, 'certificationTestCourse', '')}
+          resultTransformer={{
+            text: 'name',
+            value: 'id',
+          }}
         />
       </Column>
     </Row>
