ba3475bdcc7dab9ab3886020ec5100d7ba286bbe
Juciel de Freitas <freitas.juciel@gmail.com>
2018-01-13 11:06:40 -0200

    fix charts color

--- src/core/chart/BarChart.jsx
+++ src/core/chart/BarChart.jsx
@@ -3,13 +3,20 @@ import PropTypes from 'prop-types';
 import { Bar } from 'react-chartjs-2';
 import colors from './colors';
 
+const hexToRgb = (hex, opacity) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ?
+    `rgba(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}, ${opacity})`
+    : null;
+};
+
 const BarChart = props => (
   <Bar
     height={props.height}
     data={{
       labels: props.labels,
       datasets: props.dataFormat ? props.dataFormat.map((format, index) => ({
-        backgroundColor: colors[index],
+        backgroundColor: hexToRgb(colors[index], '0.3'),
         borderColor: colors[index],
         pointHoverRadius: 0,
         pointBorderWidth: 0,
