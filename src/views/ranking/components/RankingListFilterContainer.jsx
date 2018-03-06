8057af78e53f9fe69480ea2acc9b4ea3c941dfd0
Juciel de Freitas <freitas.juciel@gmail.com>
2018-03-06 18:56:02 -0300

    fix to reset filters when. mounting it

--- src/views/ranking/components/RankingListFilterContainer.jsx
+++ src/views/ranking/components/RankingListFilterContainer.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import RankingListFilter from './RankingListFilter';
 import RankingListFilterService from '../services/RankingListFilterService';
 
-const RankingListFilterContainer = () => (
-  <RankingListFilter
-    values={RankingListFilterService.form.getValues()}
-    errors={RankingListFilterService.form.errors}
-    onChange={RankingListFilterService.form.setValue}
-    onSearch={RankingListFilterService.handleSearch}
-  />
-);
+class RankingListFilterContainer extends Component {
+
+  componentWillMount() {
+    RankingListFilterService.init();
+  }
+
+  render() {
+    return (
+      <RankingListFilter
+        values={RankingListFilterService.form.getValues()}
+        errors={RankingListFilterService.form.errors}
+        onChange={RankingListFilterService.form.setValue}
+        onSearch={RankingListFilterService.handleSearch}
+      />
+    );
+  }
+}
 
 export default observer(RankingListFilterContainer);
