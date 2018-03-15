b6b9feae8e83664b39e17f4a38c40cdc7deb35e7
Juciel de Freitas <freitas.juciel@gmail.com>
2018-03-15 15:09:31 -0300

    Added company demo student limit

--- src/views/company/services/CompanyFormService.js
+++ src/views/company/services/CompanyFormService.js
@@ -17,6 +17,7 @@ export default class CompanyFormService {
       name: [isRequired],
       state: [isRequired],
       distributor: [isRequired],
+      demoStudentLimit: [isRequired],
       cnpj: [isRequired, isCNPJ],
     };
   }
