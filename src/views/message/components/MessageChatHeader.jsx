5e02711b977aa03b2145eaab8ca460c9d6c871cb
Juciel de Freitas <freitas.juciel@gmail.com>
2018-10-10 14:35:01 -0300

    added link to records and added student classrom

--- src/views/message/components/MessageChatHeader.jsx
+++ src/views/message/components/MessageChatHeader.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
+import head from 'lodash/head';
+import { Link } from 'react-router';
 
 const MessageChatHeader = props => (
   <div
@@ -18,6 +20,22 @@ const MessageChatHeader = props => (
       }}
     >
       {props.members.reduce((acc, member) => acc.concat(' ').concat(member.name), '')}
+    </span>
+    <Link
+      to={`/records/schools/${head(props.members).schoolClass.school}/classes/${head(props.members).schoolClass.id}/students/${head(props.members).id}/detail`}
+      target="_blank"
+    >
+      - Check student records
+    </Link>
+    <br />
+    <span>
+      Classroom: {head(props.members).schoolClass.name}
+      <Link
+        to={`/records/schools/${head(props.members).schoolClass.school}/classes/${head(props.members).schoolClass.id}/students`}
+        target="_blank"
+      >
+      - Check classroom records
+      </Link>
     </span>
     <br />
     <span>Subject: {props.subject}</span>
