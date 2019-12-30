6ab8b597abde32d5252d2a720e59371181104487
Juciel de Freitas <freitas.juciel@gmail.com>
2019-12-30 15:00:30 -0300

    change to send to check the text with corrections and to show the dictionary when it already has values

--- src/views/item/services/SpellCheckService.js
+++ src/views/item/services/SpellCheckService.js
@@ -11,7 +11,12 @@ export default class SpellCheckService {
     });
   }
 
-  handleSubmit = action((text) => {
+  handleSubmit = action((text, dictionary) => {
+    if (dictionary) {
+      Object.keys(dictionary).forEach(word => {
+        text = text.replace(word, dictionary[word])
+      });
+    }
     this.submit.fetch({
       method: 'post',
       url: '/speech-recognition/validate',
