38d28401538c158f483885496820d309e5653b8a
Juciel de Freitas <freitas.juciel@gmail.com>
2020-10-07 14:48:51 -0300

    added active students count by month chart

---
+++ src/views/dashboard/components/common/history/ActiveStudentsByMonthChart.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from '../../../../../core/layout/Card';
+import BarChart from '../../../../../core/chart/BarChart';
+import colors from '../../../../../core/chart/colors';
+
+const ActiveStudentsByMonthChart = props => (
+  <Card
+    title="Active Students By Month"
+    loading={props.loading}
+  >
+    <BarChart
+      height={350}
+      labels={props.data.map(d => d.id)}
+      data={props.data}
+      colors={colors}
+      dataFormat={[
+        {
+          label: 'Total',
+          valueRender: item => item.activeStudentsCount || 0,
+        },
+      ]}
+    />
+  </Card>
+);
+
+ActiveStudentsByMonthChart.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  data: PropTypes.array,
+};
+
+ActiveStudentsByMonthChart.defaultProps = {
+  data: [],
+};
+
+export default ActiveStudentsByMonthChart;
---
+++ src/views/dashboard/components/common/history/ActiveStudentsByMonthChartContainer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+import { toJS } from 'mobx';
+import ActiveStudentsByMonthChart from './ActiveStudentsByMonthChart';
+import ActiveStudentsByMonthService from '../../../services/ActiveStudentsByMonthService';
+
+class ActiveStudentsByMonthChartContainer extends React.PureComponent {
+
+  componentDidMount() {
+    ActiveStudentsByMonthService.load();
+  }
+
+  render() {
+    return (
+      <ActiveStudentsByMonthChart
+        data={toJS(ActiveStudentsByMonthService.data)}
+        loading={ActiveStudentsByMonthService.fetch.fetching}
+      />
+    );
+  }
+}
+
+export default observer(ActiveStudentsByMonthChartContainer);
